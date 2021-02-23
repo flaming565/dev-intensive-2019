c31cdfa994966f2d90c215f0ce660617fb155b3c
flaming565 <aladyshevaanna@mail.ru>
2021-02-23 17:55:15 +0300

    Change enums values

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,21 +2,17 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context.INPUT_METHOD_SERVICE
-import android.content.res.Resources
 import android.graphics.Rect
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import android.opengl.ETC1.getHeight
 
 
 fun Activity.hideKeyboard() {
-    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
 }
 
-private fun dip(value: Int): Int {
-    return (value * Resources.getSystem().displayMetrics.density).toInt()
-}
 
 fun Activity.isKeyboardOpen(): Boolean {
     val rootView = window.decorView.rootView
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -16,11 +16,10 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         if (validationMessage.isEmpty()) {
             return if (question.answers.contains(answer.toLowerCase())) {
                 question = question.nextQuestion()
-                "Отлично - это правильный ответ!\n${question.question}" to status.color
+                "Отлично - ты справился\n${question.question}" to status.color
             } else {
                 status = status.nextStatus()
-                "Это не правильный ответ!\n" +
-                        "${question.question}" to status.color
+                "Это неправильный ответ\n${question.question}" to status.color
             }
         } else
             return "$validationMessage\n${question.question}" to status.color
@@ -80,7 +79,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
-        CRITICAL(Triple(255, 255, 0));
+        CRITICAL(Triple(255, 0, 0));
 
         fun nextStatus(): Status {
             return if (this.ordinal < values().lastIndex)
@@ -91,7 +90,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     enum class Question(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+        NAME("Как меня зовут?", listOf("Бендер", "bender"))  {
             override fun nextQuestion(): Question = PROFESSION
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
